d79d39597dc069324c872d13949786a24a39532c
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2026-03-02 12:48:05 +0700

    final

--- lab11no2.c
+++ lab11no2.c
@@ -1,78 +1,111 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define NO_SCORE 5
+#define NO_STD 10
+
 typedef struct {
-char name[16];
-char surname[20];
-int score[NO_SCORE];
-float total;
+    char name[16];
+    char surname[20];
+    int score[NO_SCORE];
+    float total;
 } Student;
-int i;
-void readStudentData(Student *);
-void findTotalScore(Student *);
-float findAverage(Student);
-void findLessThanTen(Student);
-void addRecordscore(Student *pStd);
+void writeStudentToFile();
+void readAndProcessData();
+float findAverage(Student s);
+void findLessThanTen(Student s);
 
 int main() {
-	Student std;
-	float avg;
-	readStudentData(&std);
-	findTotalScore(&std);
-	avg = findAverage(std);
-	addRecord(&std);
-	printf("\n\nAverage score is %.2f", avg);
-	findLessThanTen(std);
-}
-void readStudentData(Student *pStd) {
 
-printf("Enter student data\n");
-printf("\tName : ");
-scanf("%s", &pStd->name);
-printf("\tSurname : ");
-scanf("%s", &pStd->surname);
-for (i=0; i<NO_SCORE; i++) {
-printf("\tScore %d : ", i+1);
-scanf("%d", &pStd->score[i]);
-	addRecordscore(&pStd);
+    writeStudentToFile();
+
+
+    readAndProcessData();
+
+    return 0;
 }
+
+void writeStudentToFile() {
+    FILE *fptr;
+    Student std;
+    int i, j;
+
+
+    fptr = fopen(".\\lab11\\no2\\std10.dat", "w"); 
+    if (fptr == NULL) {
+        printf("Error: Cannot create file.\n");
+        exit(1);
+    }
+
+    for (i = 0; i < NO_STD; i++) {
+        printf("Enter data for student %d\n", i + 1);
+        printf("\tName : "); scanf("%s", std.name);
+        printf("\tSurname : "); scanf("%s", std.surname);
+        
+
+        fprintf(fptr, "%s %s", std.name, std.surname);
+        
+        for (j = 0; j < NO_SCORE; j++) {
+            printf("\tScore %d : ", j + 1);
+            scanf("%d", &std.score[j]);
+
+            fprintf(fptr, " %d", std.score[j]);
+        }fprintf(fptr, "\n");
+
+        printf("---------------------------\n");
+    }
+    fclose(fptr);
+    printf("\nSaved\n");
 }
-void findTotalScore(Student *pStd) {
-int i;
-printf("\n\nPrint student data");
-printf("\n\t%s %s got score ", pStd->name, pStd->surname);
-pStd->total = 0.0;
-for (i=0; i<NO_SCORE; i++) {
-printf("%6d", pStd->score[i]);
-pStd->total += pStd->score[i];
+
+void readAndProcessData() {
+    FILE *fptr;
+    Student std;
+    int i;
+    float avg;
+
+
+    fptr = fopen(".\\lab11\\no2\\std10.dat", "r");
+    if (fptr == NULL) {
+        printf("Error: Could not open file for reading.\n");
+        return;
+    }
+while (fscanf(fptr, "%s %s", std.name, std.surname) != EOF) {
+        
+
+        std.total = 0;
+        for (i = 0; i < NO_SCORE; i++) {
+            fscanf(fptr, "%d", &std.score[i]);
+            std.total += std.score[i];
+        }
+
+   
+        avg = findAverage(std);
+
+        printf("\nStudent: %s %s", std.name, std.surname);
+        printf("\n  - Total Score: %.2f", std.total);
+        printf("\n  - Average Score: %.2f", avg);
+
+        
+        findLessThanTen(std);
+        printf("\n-------------------------------");
+    }
+
+    fclose(fptr);
 }
-printf("\n\tTotal score is %.2f", pStd->total);
-}
+
 float findAverage(Student s) {
-return(s.total/NO_SCORE);
-}
-void findLessThanTen(Student s) {
-int i,count=0;
-printf("\n\nScore less than 10");
-for (i=0; i<NO_SCORE; i++) {
-if (s.score[i] < 10) {
-printf("\n\tTest no.%d - %d", i+1, s.score[i]);
-count++;
-}
-}
-if (count==0) 
-printf(" -> None");
-}
-//
-void addRecord(Student *pStd) {
-	FILE *fptr;
-	fptr=fopen(".\\lab11\\no2\\std10.dat", "w");
-	fprintf(fptr, "%s\t%s\t",pStd->name,pStd->surname);
-	fclose(fptr);
+    return (s.total / NO_SCORE);
 }
 
-void addRecordscore(Student *pStd) {
-	FILE *fptr;
-	fptr=fopen(".\\lab11\\no2\\std10.dat", "a");
-	fprintf(fptr, "score:%d %d\n",i,pStd->score);
-	fclose(fptr);
+void findLessThanTen(Student s) {
+    int i, count = 0;
+    printf("\n  - Scores less than 10:");
+    for (i = 0; i < NO_SCORE; i++) {
+        if (s.score[i] < 10) {
+            printf("\n\tTest #%d: %d", i + 1, s.score[i]);
+            count++;
+        }
+    }
+    if (count == 0) printf(" None");
 }
